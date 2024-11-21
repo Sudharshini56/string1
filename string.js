3a4ca0b0d32d993cf37a347804c541ea26351e45
Sudharshini56 <bala.sudharshini@gmail.com>
2024-11-21 11:36:25 +0530

    string.js

--- string.js
+++ string.js
@@ -50,3 +50,44 @@ console.log(str2.includes("never"))
 console.log(str2.startsWith("but"))
 console.log(str2.endsWith("up"))
 console.log(str2.replace(/give/g, "never"))
+
+
+//Javascript logic
+const amount = 1000
+console.log("your bill amount is Rs. ", amount)
+
+
+
+//sting
+
+let str3 = "never give up"
+let str4 = "but you can"
+console.log(str3 + str4)
+console.log(str3.concat(str4))
+console.log(str3.concat("sudharshini", str4))
+console.log(str3.slice(4, 10))
+console.log(str3.substring(4, 10))
+console.log(str3.substr(4, 10))
+console.log(str3.toLocaleLowerCase())
+console.log(str3.toLocaleUpperCase())
+console.log(str3.startsWith("b"))
+console.log(str3.endsWith("n"))
+console.log(str3.replace("but", "sudharshini"))
+console.log(str3.replaceAll("you", "we"))
+console.log(str3.length())
+console.log.log(str3.charAt(1))
+console.log(str3[1])
+console.log(str3.charCodeAt(1))
+console.log(str3.indexOf("c"))
+console.log(str3.lastIndexOf("a"))
+console.log(str3.includes("can"))
+console.log(str3.includes("not"))
+console.log(str3.repeat("can"))
+let billl = "Rs.10"
+console.log(bill.padStart(10, "0"))
+console.log(billl.length())
+console.log(billl.padEnd(20, "paid"))
+console.log(str4.search("can"))
+console.log(str4.trim())
+console.log(str4.trimEnd())
+console.log(str4.trimStart())
