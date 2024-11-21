2f43d040cc489b2465cb15afe30c91f365a50ce6
Sudharshini56 <bala.sudharshini@gmail.com>
2024-11-21 14:40:14 +0530

    use `

--- string.js
+++ string.js
@@ -1,93 +1,108 @@
-let str1 = "if you want to try this"
-let str2 = "but never give up  "
+// let str1 = "if you want to try this"
+// let str2 = "but never give up  "
 
-console.log(str1 + str2)
+// console.log(str1 + str2)
 
-console.log(str1.concat(str2))
+// console.log(str1.concat(str2))
 
-console.log(str1.concat("** **", str2))
-
-
-// How to use part part in string
-
-//sclice(start,end)
-//substring(start,end)
-//substr(start,length)
-
-console.log(str2.slice(4, 10))
-console.log(str2.substring(4.10))
-console.log(str2.substr(4, 10))
-console.log(str2.substr(-4, 10))
-console.log(str2.replace("never", "sudharshini"))
-console.log(str2.replace("Up", "give"))
-console.log(str2.replaceAll("u", "sudharshini "))
-console.log(str2.toLowerCase())
-console.log(str2.toUpperCase())
-console.log(str2.length)
-console.log(str2.trim())
-console.log(str2.trimStart())
-console.log(str2.trimEnd())
-
-let bill = 'Rs.10'
-console.log(bill.length)
-console.log(bill.padStart(7, '0'))
-console.log(bill.padStart(10, "wwwww"))
-console.log(bill.padStart(3, "Rs"))
-console.log(bill.padEnd(9, "paid"))
+// console.log(str1.concat("** **", str2))
 
 
+// // How to use part part in string
 
-console.log(str2[1])
-console.log(str2[10])
-console.log(str2.charAt(1))
-console.log(str2.charCodeAt(1))
+// //sclice(start,end)
+// //substring(start,end)
+// //substr(start,length)
 
-console.log(str2.indexOf("b"))
-console.log(str2.lastIndexOf("u"))
-console.log(str2.search("never"))
-console.log(str2.search("nott"))
-console.log(str2.includes("never"))
-console.log(str2.startsWith("but"))
-console.log(str2.endsWith("up"))
-console.log(str2.replace(/give/g, "never"))
+// console.log(str2.slice(4, 10))
+// console.log(str2.substring(4.10))
+// console.log(str2.substr(4, 10))
+// console.log(str2.substr(-4, 10))
+// console.log(str2.replace("never", "sudharshini"))
+// console.log(str2.replace("Up", "give"))
+// console.log(str2.replaceAll("u", "sudharshini "))
+// console.log(str2.toLowerCase())
+// console.log(str2.toUpperCase())
+// console.log(str2.length)
+// console.log(str2.trim())
+// console.log(str2.trimStart())
+// console.log(str2.trimEnd())
 
-
-//Javascript logic
-const amount = 1000
-console.log("your bill amount is Rs. ", amount)
+// let bill = 'Rs.10'
+// console.log(bill.length)
+// console.log(bill.padStart(7, '0'))
+// console.log(bill.padStart(10, "wwwww"))
+// console.log(bill.padStart(3, "Rs"))
+// console.log(bill.padEnd(9, "paid"))
 
 
 
-//sting
+// console.log(str2[1])
+// console.log(str2[10])
+// console.log(str2.charAt(1))
+// console.log(str2.charCodeAt(1))
 
-let str3 = "never give up"
-let str4 = "but you can"
-console.log(str3 + str4)
-console.log(str3.concat(str4))
-console.log(str3.concat("sudharshini", str4))
-console.log(str3.slice(4, 10))
-console.log(str3.substring(4, 10))
-console.log(str3.substr(4, 10))
-console.log(str3.toLocaleLowerCase())
-console.log(str3.toLocaleUpperCase())
-console.log(str3.startsWith("b"))
-console.log(str3.endsWith("n"))
-console.log(str3.replace("but", "sudharshini"))
-console.log(str3.replaceAll("you", "we"))
-console.log(str3.length())
-console.log.log(str3.charAt(1))
-console.log(str3[1])
-console.log(str3.charCodeAt(1))
-console.log(str3.indexOf("c"))
-console.log(str3.lastIndexOf("a"))
-console.log(str3.includes("can"))
-console.log(str3.includes("not"))
-console.log(str3.repeat("can"))
-let billl = "Rs.10"
-console.log(bill.padStart(10, "0"))
-console.log(billl.length())
-console.log(billl.padEnd(20, "paid"))
-console.log(str4.search("can"))
-console.log(str4.trim())
-console.log(str4.trimEnd())
-console.log(str4.trimStart())
+// console.log(str2.indexOf("b"))
+// console.log(str2.lastIndexOf("u"))
+// console.log(str2.search("never"))
+// console.log(str2.search("nott"))
+// console.log(str2.includes("never"))
+// console.log(str2.startsWith("but"))
+// console.log(str2.endsWith("up"))
+// console.log(str2.replace(/give/g, "never"))
+
+
+// //Javascript logic
+// const amount = 1000
+// console.log("your bill amount is Rs. ", amount)
+
+
+
+// //sting
+
+// let str3 = "never give up"
+// let str4 = "but you can"
+// console.log(str3 + str4)
+// console.log(str3.concat(str4))
+// console.log(str3.concat("sudharshini", str4))
+// console.log(str3.slice(4, 10))
+// console.log(str3.substring(4, 10))
+// console.log(str3.substr(4, 10))
+// console.log(str3.toLocaleLowerCase())
+// console.log(str3.toLocaleUpperCase())
+// console.log(str3.startsWith("b"))
+// console.log(str3.endsWith("n"))
+// console.log(str3.replace("but", "sudharshini"))
+// console.log(str3.replaceAll("you", "we"))
+// console.log(str3.length())
+// console.log.log(str3.charAt(1))
+// console.log(str3[1])
+// console.log(str3.charCodeAt(1))
+// console.log(str3.indexOf("c"))
+// console.log(str3.lastIndexOf("a"))
+// console.log(str3.includes("can"))
+// console.log(str3.includes("not"))
+// console.log(str3.repeat("can"))
+// let billl = "Rs.10"
+// console.log(bill.padStart(10, "0"))
+// console.log(billl.length())
+// console.log(billl.padEnd(20, "paid"))
+// console.log(str4.search("can"))
+// console.log(str4.trim())
+// console.log(str4.trimEnd())
+// console.log(str4.trimStart())
+
+let firstName = "Sudharshini"
+let lastName = "Sudharshini"
+let county = "USA"
+console.log(`My name is ${firstName} ${lastName} I Live in ${county}`)
+
+let wish = `Good
+Morning`
+console.log(`${wish}`)
+
+let birthday = `Happy birthday
+to you
+sudharshini
+2024`
+console.log(`${birthday}`)
