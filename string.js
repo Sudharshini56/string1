c998a1f6afb901c74ca0d5c7f5b1eb2b1c6f0454
Sudharshini56 <bala.sudharshini@gmail.com>
2024-11-22 21:44:45 +0530

    if

--- string.js
+++ string.js
@@ -18,3 +18,19 @@ if (isBannedUser) {
 
 
 console.log(reply)
+
+
+
+let testScore = 88;
+let grade;
+
+if (testScore >= 90) {
+    grade = "A"
+} else if (testScore >= 80) {
+    grade = "B"
+} else if (testScore >= 70) {
+    grade = "c"
+} else if (testScore >= 60) {
+    grade = "D"
+}
+console.log(grade)
