27bb7ab1012b97bc27e86fa0f1cce01f2f468134
Sudharshini56 <bala.sudharshini@gmail.com>
2024-11-25 19:00:57 +0530

    loops

--- string.js
+++ string.js
@@ -1,261 +1,369 @@
-// // //IF Statement
+// // // // //IF Statement
 
-// // let isBannedUser = false
-// // let viewer = "Js course"
-// // let finnane = true
-// // let reply;
+// // // // let isBannedUser = false
+// // // // let viewer = "Js course"
+// // // // let finnane = true
+// // // // let reply;
 
 
-// // if (isBannedUser) {
-// //     reply = 'sorry'
-// // } else if (viewer && finnane) {
-// //     reply = `you can buy this ${viewer} and Enjoy finnane connent too`
-// // } else if (viewer) {
-// //     reply = `Enjoy ${viewer}`
-// // } else {
-// //     reply = 'Sorry still working'
+// // // // if (isBannedUser) {
+// // // //     reply = 'sorry'
+// // // // } else if (viewer && finnane) {
+// // // //     reply = `you can buy this ${viewer} and Enjoy finnane connent too`
+// // // // } else if (viewer) {
+// // // //     reply = `Enjoy ${viewer}`
+// // // // } else {
+// // // //     reply = 'Sorry still working'
+// // // // }
+
+
+// // // // console.log(reply)
+
+
+
+// // // // let testScore = 88;
+// // // // let grade;
+// // // // let schoolStudent = true;
+
+// // // // if (testScore >= 90) {
+// // // //     grade = "A"
+// // // // } else if (testScore >= 80) {
+// // // //     grade = "B"
+// // // // } else if (testScore >= 70) {
+// // // //     grade = "c"
+// // // // } else if (testScore >= 60) {
+// // // //     grade = "D"
+// // // // } else {
+// // // //     if (schoolStudent) {
+// // // //         grade = "RA"
+// // // //     } else {
+// // // //         grade = "F"
+// // // //     }
+
+// // // // }
+// // // // console.log(grade)
+
+
+
+// // // let isBannedUser = true;
+// // // let viewer = "JS Course";
+// // // let reply;
+// // // let finnance = true;
+
+// // // if (isBannedUser) {
+// // //     reply = "sorry you do not have access"
+// // // } else if (viewer && finnance) {
+// // //     reply = `Enjoy ${viewer} and find your finnace contantent`
+
+// // // } else if (viewer) {
+// // //     reply = `Enjoy ${viewer}`
+// // // } else {
+// // //     reply = "Still pending"
+// // // }
+
+// // // console.log(reply)
+
+
+// // // let testScore = 99
+// // // let grade;
+// // // let schoolStudent = true;
+
+// // // if (testScore >= 88) {
+// // //     grade = "A"
+// // // } else if (testScore >= 77) {
+// // //     grade = "B"
+// // // } else if (testScore >= 66) {
+// // //     grade = "c"
+// // // } else if (testScore >= 55) {
+// // //     grade = "D"
+// // // } else {
+// // //     if (schoolStudent) {
+// // //         reply = "RA"
+// // //     } else {
+// // //         grade = "F"
+// // //     }
+
+// // // }
+
+// // // //Switch 
+
+// // // switch ("1") {
+// // // //     case 1:
+// // // //         console.log(1)
+// // // //         break;
+// // // //     case ("2"):
+// // // //         console.log(2)
+// // // //         break;
+// // // //     default:
+// // // //         console.log("No match")
+// // // // }
+
+// // // // switch (Math.floor(Math.random() * 4)) {
+// // // //     case 1:
+// // // //         console.log(1);
+// // // //         break;
+// // // //     case 2:
+// // // //         console.log(2);
+// // // //         break;
+// // // //     default:
+// // // //         console.log("No match")
+
+// // // // }
+
+
+// // // // switch (Math.floor(Math.random() * 100)) {
+// // // //     case 1:
+// // // //         console.log(1);
+// // // //         break;
+// // // //     case 2:
+// // // //         console.log(2);
+// // // //         break;
+// // // //     default:
+// // // //         console.log("Try again")
+// // // // }
+
+
+
+
+
+// // // // let player = "rock";
+// // // // let computer = "scissors";
+
+
+// // // // switch (player) {
+// // // //     case computer:
+// // // //         console.log("Tie");
+// // // //         break;
+
+// // // //     case "rock":
+// // // //         if (computer === "scissors") {
+// // // //             console.log("player's win");
+// // // //         }
+// // // //         break;
+
+// // // //     case "scissors":
+// // // //         if (player === "paper") {
+// // // //             console.log("player's win");
+// // // //         }
+// // // //         break;
+
+// // // //     case "paper":
+// // // //         if (computer === "scirssors") {
+// // // //             console.log("player's win");
+// // // //         }
+// // // //         break;
+
+// // // //     default:
+// // // //         console.log("No Tie!");
+// // // // }
+
+
+
+// // // // let subcriber = "Subcribed";
+// // // // let response = subcriber ? "welcome" : "kindly subcribe";
+// // // // console.log(response);
+
+// // // // let testScore = 45;
+// // // // let grade = testScore > 88 ? "A" : testScore > 77 ? "B" : testScore > 66 ? "C" : testScore > 55 ? "D" : "F";
+// // // // console.log(g)
+
+
+// // // // let subcriber = "subcribed";
+// // // // let response = subcriber ? "welcome" : "kindlt subcriber";
+// // // // console.log(response)
+
+// // // // let testScore = 88;
+// // // // let grade = testScore > 85 ? "A" : testScore > 75 ? "B" : testScore > 65 ? "C" : testScore > 55 ? "D" : "F";
+// // // // console.log(grade)
+
+
+
+// // // // let myBlooan = alert("Hi Subcribers")
+// // // // console.log(myBlooan)
+
+// // // // let my = confirm("Hi subcribers");
+// // // // console.log(my)
+
+// // // let my = prompt("Enter your name: ");
+// // // if (my) {
+// // //     console.log(my ? ? "you did not enter your name");
+// // // } else {
+// // //     console.log("Try again")
+// // // }
+// // // console.log(my.trim().length);
+
+// // // let user = alert("Enter your name: ")
+
+// // // let user = confirm("Enter your name: ")
+// // // console.log(user)
+
+// // // let user = prompt("Enter your name: ");
+// // // if (user) {
+// // //     console.log(user ? ? "You didn't Enter name");
+// // // } else {
+// // //     console.log("You did not enter name");
+// // // }
+// // // console.log(user.trim().length);
+
+
+
+// // // Loops
+// // // while loop
+// // // let i = 1;
+// // // while (i <= 50) {
+// // //     console.log(i);
+// // //     i++;
+// // // }
+// // // while loop
+
+// // // let i = 1;
+// // // while (i <= 100) {
+// // //     console.log(i)
+// // //     i = i + 11
+// // // }
+
+// // // do while loop
+// // // atlest onece
+
+// // // let i = 50;
+// // // do {
+// // //     console.log(i);
+// // //     i++;
+
+// // // } while (i < 50);
+
+
+// // // while loop
+
+// // // let i = 1;
+// // // while (i <= 50) {
+// // //     console.log(i);
+// // //     i++;
+// // // }
+
+// // // do while loop
+
+// // // let i = 1;
+// // // do {
+// // //     console.log(i);
+// // //     i++;
+// // // } while (i <= 50)
+
+
+// // // //for loop
+// // // for (i = 1; i <= 50; i++) {
+// // //     console.log(i)
+// // // }
+
+// // // let name = "Sudharshini"
+// // // for (i = 0; i <= name.length; i++) {
+// // //     console.log(name.charAt(i));
+// // // }
+
+// // // let name = "Sudharshini"
+// // // for (i = 0; i <= name.length; i++) {
+// // //     console.log(name.charAt(i));
+// // //     for (j = 0; j <= name.length; j++) {
+// // //         console.log(name.charAt(j), name.charAt(i))
+// // //     }
+// // // }
+
+// // let i
+
+// // for (i = 1; i <= 5; i++) {
+// //     console.log("Hello")
 // // }
-
-
-// // console.log(reply)
+// // console.log('outside loop', i)
 
 
 
-// // let testScore = 88;
-// // let grade;
-// // let schoolStudent = true;
+// // for (i = 1; i <= 5; i++) {
+// //     console.log(i)
+// // }
+// // console.log('outside loop', i)
 
-// // if (testScore >= 90) {
-// //     grade = "A"
-// // } else if (testScore >= 80) {
-// //     grade = "B"
-// // } else if (testScore >= 70) {
-// //     grade = "c"
-// // } else if (testScore >= 60) {
-// //     grade = "D"
-// // } else {
-// //     if (schoolStudent) {
-// //         grade = "RA"
-// //     } else {
-// //         grade = "F"
+
+// // let name = "Sudharshini"
+// // for (i = 0; i <= name.length; i++) {
+// //     console.log(name.charAt(i));
+
+// //     for (j = 0; j <= name.length; j++) {
+// //         console.log(name.charAt(i), name.charAt(j))
 // //     }
-
 // // }
-// // console.log(grade)
+// // console.log(j)
+// // console.log(i)
 
-
-
-// let isBannedUser = true;
-// let viewer = "JS Course";
-// let reply;
-// let finnance = true;
-
-// if (isBannedUser) {
-//     reply = "sorry you do not have access"
-// } else if (viewer && finnance) {
-//     reply = `Enjoy ${viewer} and find your finnace contantent`
-
-// } else if (viewer) {
-//     reply = `Enjoy ${viewer}`
-// } else {
-//     reply = "Still pending"
-// }
-
-// console.log(reply)
-
-
-// let testScore = 99
-// let grade;
-// let schoolStudent = true;
-
-// if (testScore >= 88) {
-//     grade = "A"
-// } else if (testScore >= 77) {
-//     grade = "B"
-// } else if (testScore >= 66) {
-//     grade = "c"
-// } else if (testScore >= 55) {
-//     grade = "D"
-// } else {
-//     if (schoolStudent) {
-//         reply = "RA"
-//     } else {
-//         grade = "F"
-//     }
-
-// }
-
-// //Switch 
-
-// switch ("1") {
-// //     case 1:
-// //         console.log(1)
-// //         break;
-// //     case ("2"):
-// //         console.log(2)
-// //         break;
-// //     default:
-// //         console.log("No match")
+// // for (i = 10; i >= 0; i--) {
+// //     console.log(i)
 // // }
 
-// // switch (Math.floor(Math.random() * 4)) {
-// //     case 1:
-// //         console.log(1);
-// //         break;
-// //     case 2:
-// //         console.log(2);
-// //         break;
-// //     default:
-// //         console.log("No match")
-
+// // i = 0;
+// // while (i >= 1) {
+// //     console.log(i);
+// //     i--;
 // // }
+// // console.log('outside loop', i)
 
 
-// // switch (Math.floor(Math.random() * 100)) {
-// //     case 1:
-// //         console.log(1);
-// //         break;
-// //     case 2:
-// //         console.log(2);
-// //         break;
-// //     default:
-// //         console.log("Try again")
-// // }
+// // i = 10
+// // do {
+// //     console.log(i)
+// //     i--
+// // } while (i >= 1)
+// // console.log('outside loop', i)
 
-
-
-
-
-// // let player = "rock";
-// // let computer = "scissors";
-
-
-// // switch (player) {
-// //     case computer:
-// //         console.log("Tie");
-// //         break;
-
-// //     case "rock":
-// //         if (computer === "scissors") {
-// //             console.log("player's win");
-// //         }
-// //         break;
-
-// //     case "scissors":
-// //         if (player === "paper") {
-// //             console.log("player's win");
-// //         }
-// //         break;
-
-// //     case "paper":
-// //         if (computer === "scirssors") {
-// //             console.log("player's win");
-// //         }
-// //         break;
-
-// //     default:
-// //         console.log("No Tie!");
-// // }
-
-
-
-// // let subcriber = "Subcribed";
-// // let response = subcriber ? "welcome" : "kindly subcribe";
-// // console.log(response);
-
-// // let testScore = 45;
-// // let grade = testScore > 88 ? "A" : testScore > 77 ? "B" : testScore > 66 ? "C" : testScore > 55 ? "D" : "F";
-// // console.log(g)
-
-
-// // let subcriber = "subcribed";
-// // let response = subcriber ? "welcome" : "kindlt subcriber";
-// // console.log(response)
-
-// // let testScore = 88;
-// // let grade = testScore > 85 ? "A" : testScore > 75 ? "B" : testScore > 65 ? "C" : testScore > 55 ? "D" : "F";
-// // console.log(grade)
-
-
-
-// // let myBlooan = alert("Hi Subcribers")
-// // console.log(myBlooan)
-
-// // let my = confirm("Hi subcribers");
-// // console.log(my)
-
-// let my = prompt("Enter your name: ");
-// if (my) {
-//     console.log(my ? ? "you did not enter your name");
-// } else {
-//     console.log("Try again")
-// }
-// console.log(my.trim().length);
-
-// let user = alert("Enter your name: ")
-
-// let user = confirm("Enter your name: ")
-// console.log(user)
-
-// let user = prompt("Enter your name: ");
-// if (user) {
-//     console.log(user ? ? "You didn't Enter name");
-// } else {
-//     console.log("You did not enter name");
-// }
-// console.log(user.trim().length);
-
-
-
-//Loops
-//while loop
-// let i = 1;
-// while (i <= 50) {
-//     console.log(i);
-//     i++;
-// }
-//while loop
-
-// let i = 1;
-// while (i <= 100) {
-//     console.log(i)
-//     i = i + 11
+// while (true) {
+//     let num = Number(prompt('enter a number'))
+//     if (!isNaN(num))
+//         break;
 // }
 
-//do while loop
-//atlest onece
 
-// let i = 50;
-// do {
-//     console.log(i);
-//     i++;
+// //continue -skips
 
-// } while (i < 50);
-
-
-//while loop
-
-// let i = 1;
-// while (i <= 50) {
-//     console.log(i);
-//     i++;
-// }
-
-//do while loop
-
-// let i = 1;
-// do {
-//     console.log(i);
-//     i++;
-// } while (i <= 50)
-
-
-// //for loop
-// for (i = 1; i <= 50; i++) {
+// for (i = 1; i <= 10; i++) {
+//     if (i % 3 == 0)
+//         continue
 //     console.log(i)
 // }
 
-let name = "Sudharshini"
-for (i = 0; i <= name.length; i++) {
-    console.log(name.charAt(i));
+
+let text = '';
+for (i = 0; i < 10; i++) {
+    if (i === 3) {
+        continue;
+    }
+    text = text + i;
 }
+console.log(text)
+
+let demo = '';
+
+for (i = 0; i <= 10; i++) {
+    if (i <= 3) {
+        continue;
+    }
+    demo = demo + i;
+}
+console.log(demo)
+
+let pop = '';
+for (i = 0; i <= 5; i++) {
+    if (i < 4) {
+        continue;
+    }
+    pop = pop + i;
+}
+console.log(pop)
+
+
+
+let popp = '';
+for (i = 0; i <= 5; i++) {
+    if (i < 4) {
+        break;
+    }
+    pop = popp + i;
+}
+console.log(popp)
